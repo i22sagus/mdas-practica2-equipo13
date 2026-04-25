d7ca0b6bb724dff8bf2de053bfb8144233288b7c
i82govij <94856380+i82govij@users.noreply.github.com>
2026-04-25 09:36:12 +0000

    MDAS: Corregir error de sintaxis en CollectionUtils - duplicación de método

--- src/main/java/org/apache/commons/collections4/CollectionUtils.java
+++ src/main/java/org/apache/commons/collections4/CollectionUtils.java
@@ -320,8 +320,7 @@ public class CollectionUtils {
     public static <C> boolean addAll(final Collection<C> collection, final Iterable<? extends C> iterable) {
         Objects.requireNonNull(collection, "collection");
         Objects.requireNonNull(iterable, "iterable");
-        public static <C> boolean addAll(final Collection<C> collection, final Iterable<? extends C> iterable) {
-      /* * MDAS: Se elimina la línea en blanco al inicio del cuerpo de la función. 
+        /* MDAS: Se elimina la línea en blanco al inicio del cuerpo de la función. 
          * No debe haber separación vertical con su cuerpo (Regla de Formato 2). 
          */
         if (iterable instanceof Collection<?>) {
